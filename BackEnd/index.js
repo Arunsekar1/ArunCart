ee917c7912fb31c87f89b9268f91a9377878420d
Arun <arunsekar071@gmail.com>
2025-08-29 12:53:40 +0530

    port changes

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -4,8 +4,10 @@ const connectDatabase = require('./config/database');
 
 connectDatabase();
 
-const server = app.listen(process.env.PORT, () => {
-    console.log(`server runnig to the port :${process.env.PORT} in ${process.env.NODE_ENV}`)
+const PORT = process.env.PORT || 7000;
+
+const server = app.listen(PORT, () => {
+    console.log(`server runnig to the port :${PORT} in ${process.env.NODE_ENV}`)
 })
 
 process.on('unhandledRejection', (err) => {
